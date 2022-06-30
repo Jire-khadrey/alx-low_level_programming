be3fcf7e1340adc1c40568a965ff4e1c947deb52
Shittu Rasheed Olajire <shitturasheed699@gmail.com>
2022-06-30 06:52:53 -0700

    C programming begins

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+/**
+ *main - prints exactly the sentence,
+ *with proper grammar,
+ *but the outcome is a piece of art,
+ *Return: 0 if processed properly.
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
