27a937a52eec19b48ca98585ee89cf3d0f9d4fc6
Shittu Rasheed Olajire <shitturasheed699@gmail.com>
2022-07-01 07:47:35 -0700

    Hello

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,5 +19,4 @@ else if (n == 0)
 printf("%d is zero\n", n);
 else if  (n < 0)
 printf("%d is negative\n", n);
-return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-File 0 generates a random number and stores it in a variable N
+all fixes
