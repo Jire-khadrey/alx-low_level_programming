a519b872c738b1c077a6641e130fb85c0823820b
Jire-khadrey <68562717+Jire-khadrey@users.noreply.github.com>
2022-07-07 17:20:30 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"                                                                                                                                      
-                                                                                                                                                       
-/**                                                                                                                                                    
- * print_numbers - Check the main file                                                                                                                 
- * Description: A function that prints the numbers, from 0 to 9                                                                                        
- * Return: Always 0                                                                                                                                    
- */                                                                                                                                                    
-                                                                                                                                                       
-void print_numbers(void)                                                                                                                               
-{                                                                                                                                                      
-        int i;                                                                                                                                         
-                                                                                                                                                       
-        for (i = 0; i <= 9; i++)                                                                                                                       
-                _putchar(i);                                                                                                                           
-        _putchar('\n');                                                                                                                                
-}   
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
