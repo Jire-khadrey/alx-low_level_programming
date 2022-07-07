ac6c39690ce0b6aa9930e3fe38f5ea53b492904c
Jire-khadrey <68562717+Jire-khadrey@users.noreply.github.com>
2022-07-07 17:18:41 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
-#include "main.h"
-
-/**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
- */
-void print_numbers(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i);
-	}
-
-	_putchar('\n');
-}
+#include "main.h"                                                                                                                                      
+                                                                                                                                                       
+/**                                                                                                                                                    
+ * print_numbers - Check the main file                                                                                                                 
+ * Description: A function that prints the numbers, from 0 to 9                                                                                        
+ * Return: Always 0                                                                                                                                    
+ */                                                                                                                                                    
+                                                                                                                                                       
+void print_numbers(void)                                                                                                                               
+{                                                                                                                                                      
+        int i;                                                                                                                                         
+                                                                                                                                                       
+        for (i = 0; i <= 9; i++)                                                                                                                       
+                _putchar(i);                                                                                                                           
+        _putchar('\n');                                                                                                                                
+}   
